9aa497b8d0ef06ba8d04d57135a03ccc56876a19
Dave <daveymason@outlook.com>
2021-01-24 15:37:20 +0800

    Added arays.js to folder
    After file and folder cahnges

---
+++ JSDataStructures/arrays.js
@@ -0,0 +1,42 @@
+//Arrays can have multiple data types. The most baic method of storing data. Above is a single dimensional array. 
+let yourArray = ['one', 2, 'three', false, 42]; 
+
+//There are also mutli-dimensial arrays that can store arrasy within arrays.
+let complexArray = [
+    [
+      {
+        one: 1,
+        two: 2
+      },
+      {
+        three: 3,
+        four: 4
+      }
+    ],
+    [
+      {
+        a: "a",
+        b: "b"
+      },
+      {
+        c: "c",
+        d: "d"
+      }
+    ]
+  ];
+
+//Access an Array's Contents Using Bracket Notation
+let myArray = ["a", "b", "c", "d"];
+
+myArray[1] = "Not B";
+
+console.log(myArray);
+
+//Add Items to an Array with push() and unshift()
+function mixedNumbers(arr) {
+  arr.push(7, 'VIII', 9);
+  arr.unshift('I', 2, 'three');
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
