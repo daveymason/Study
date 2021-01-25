58510ab41db6b1d20b9d2366e333463e864dd1b5
Dave <daveymason@outlook.com>
2021-01-25 16:39:42 +0800

    Created loops.js
    A new file to practice loops

---
+++ JSDataStructures/loops.js
@@ -0,0 +1,20 @@
+//While
+let i = 0;
+while (i=10) {
+    1 +=2;
+}
+
+//Do While
+let i = 0;
+do {
+    1+= 2;
+} while (i < 10);
+
+//For
+let count  = 10;
+function saySomething() {
+    document.writeIn("Hello!");
+}
+for(let i =0; i < count; i++){
+    saySomething();
+}
