a677f3045438be8c34133d28483b64d32cceaac0
Dave <daveymason@outlook.com>
2021-01-26 20:11:44 +0800

    Loops and Arrays together
    Return a filtered version of the passed array such that any array nested within arr containing elem has been removed

--- JSDataStructures/arrays.js
+++ JSDataStructures/arrays.js
@@ -155,3 +155,17 @@ function quickCheck(arr, elem) {
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+//Return a filtered version of the passed array such that any array nested within arr containing elem has been removed.
+function filteredArray(arr, elem) {
+  let newArr = [];
+   for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      //Checks every parameter for the element and if is NOT there continues the code
+      newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
+    }
+  }
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
