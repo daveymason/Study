700a5d994d13d532e1b7a5d25ccf8c6f5dfe17d5
Dave <daveymason@outlook.com>
2021-02-06 20:32:59 +0800

    Indexing
    The for loop was of by one as it started at 1 instead of 0

--- Debugging/debugging.js
+++ Debugging/debugging.js
@@ -42,4 +42,15 @@ function getNine() {
   let result = getNine();
   console.log(result);
 
+  //Catch the indexing off by 1 error
+  function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+
+    for (let i = 0; i < len; i++) {
+
+      console.log(firstFive[i]);
+    }
+  }
   
+  countToFive();
