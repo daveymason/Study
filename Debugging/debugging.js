5e26bc75f5d45f7e031414c0d8152f8fa33058b8
Dave <daveymason@outlook.com>
2021-02-06 20:29:23 +0800

    Double equals
    The error was the condition was only using a single equals sign.

--- Debugging/debugging.js
+++ Debugging/debugging.js
@@ -18,3 +18,16 @@ console.log(typeof three);
 //Catch the single and double quote marks
 let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
+
+//Double Equals sign
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
