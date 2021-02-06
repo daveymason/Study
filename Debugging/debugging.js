23431b5b4f0da53e084654f0a4206b3f0124c874
Dave <daveymason@outlook.com>
2021-02-06 20:30:47 +0800

    Missing brackets after a function call
    getNine() had no brackets.

--- Debugging/debugging.js
+++ Debugging/debugging.js
@@ -31,3 +31,15 @@ if(x == y) {
 }
 
 console.log(result);
+
+//Catch Missing Open and Closing Parenthesis After a Function Call
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+  console.log(result);
+
+  
