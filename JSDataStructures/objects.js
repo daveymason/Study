7c358ff93ff2eeb85be1913af47d8daa1bd38e6d
Dave <daveymason@outlook.com>
2021-02-07 17:13:53 +0800

    Check if an Object has a Property
    Check if an Object has a Property

--- JSDataStructures/objects.js
+++ JSDataStructures/objects.js
@@ -59,3 +59,40 @@ let foods = {
     delete foods.plums;
   
   console.log(foods);
+
+// Returns true only if the users object contains all four names, Alan, Jeff, Sarah, and Ryan, as keys, and false otherwise
+  let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(obj) {
+    // Only change code below this line
+    if (
+      obj.hasOwnProperty("Alan") &&
+      obj.hasOwnProperty("Jeff") &&
+      obj.hasOwnProperty("Sarah") &&
+      obj.hasOwnProperty("Ryan")
+    ) {
+      return true;
+    }
+    return false;
+  }
+    // Only change code above this line
+  
+  
+  console.log(isEveryoneHere(users));
