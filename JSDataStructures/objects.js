307d7e5490f72b70d1e94e4cf511f0721bc01ff3
Dave <daveymason@outlook.com>
2021-02-07 17:07:29 +0800

    Delete keyword
    Use delete keyword to remove properties

--- JSDataStructures/objects.js
+++ JSDataStructures/objects.js
@@ -42,3 +42,20 @@ let foods = {
   }
   
   console.log(checkInventory("apples"));
+
+
+  //use the delete keyword to remove properties
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+    delete foods.oranges;
+    delete foods.strawberries;
+    delete foods.plums;
+  
+  console.log(foods);
