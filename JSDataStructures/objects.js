72c0e888b529ef337e4169a1b539bebe6111f95b
Dave <daveymason@outlook.com>
2021-02-07 17:23:15 +0800

    Generate an Array of All Object Keys with Object.keys()
    Object.keys()

--- JSDataStructures/objects.js
+++ JSDataStructures/objects.js
@@ -106,3 +106,30 @@ let foods = {
     }
     return result;
   }
+
+
+  //Generate an Array of All Object Keys with Object.keys()
+  let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function getArrayOfUsers(obj) {
+    return Object.keys(users)
+  }
+  
+  console.log(getArrayOfUsers(users));
