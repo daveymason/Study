31e01b98a8fe6d9de1e35ddd4190d57e4db60517
Dave <daveymason@outlook.com>
2021-02-07 17:02:39 +0800

    Modify objects within objects
    Change online to 45

--- JSDataStructures/objects.js
+++ JSDataStructures/objects.js
@@ -1,15 +1,27 @@
 //Add Key-Value Pairs to JavaScript Objects
-
 let foods = {
     apples: 25,
     oranges: 32,
     plums: 28
   };
   
-  // Only change code below this line
   foods["bananas"] = 13;
   foods["grapes"] = 35;
   foods["strawberries"] = 27;
-  // Only change code above this line
   
   console.log(foods);
+
+  
+  //Modify an object within an object
+  let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  //Change online object to 45
+  userActivity.data.online = 45;
+  
+  console.log(userActivity);
