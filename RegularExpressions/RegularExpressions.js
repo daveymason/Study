1e6b410e03b66ae354b7fa5af2a0865e0859dc3a
Dave <daveymason@outlook.com>
2021-02-09 12:16:14 +0800

    Update RegularExpressions.js
    Match method

--- RegularExpressions/RegularExpressions.js
+++ RegularExpressions/RegularExpressions.js
@@ -1,4 +1,13 @@
 //Using the Test Method
 let myString = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myString); // Change this line
+let result = myRegex.test(myString); 
+
+//Using the Match Method
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; 
+let result = extractStr.match(codingRegex); 
+
+//Note that the .match syntax is the "opposite" of the .test method you have been using thus far:
+'string'.match(/regex/);
+/regex/.test('string');
