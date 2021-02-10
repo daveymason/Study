b51dfa2634f76c8ee681e457ebce0d83a3394b27
Dave <daveymason@outlook.com>
2021-02-10 23:51:31 +0800

    Update RegularExpressions.js
    Extract pattersn more than once

--- RegularExpressions/RegularExpressions.js
+++ RegularExpressions/RegularExpressions.js
@@ -11,3 +11,9 @@ let result = extractStr.match(codingRegex);
 //Note that the .match syntax is the "opposite" of the .test method you have been using thus far:
 'string'.match(/regex/);
 /regex/.test('string');
+
+
+//Using the regex starRegex, find and extract both "Twinkle" words from the string twinkleStar.
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi;  //'g' flag extracts patern more than once. 'i' flag removes case sensitivity.
+let result = twinkleStar.match(starRegex); 
