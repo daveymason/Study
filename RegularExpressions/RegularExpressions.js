e58663566113c02dd590a89f373139191c1f9a88
Dave <daveymason@outlook.com>
2021-02-11 00:53:06 +0800

    Update RegularExpressions.js
    Wildcard flag to search for everything

--- RegularExpressions/RegularExpressions.js
+++ RegularExpressions/RegularExpressions.js
@@ -12,8 +12,12 @@ let result = extractStr.match(codingRegex);
 'string'.match(/regex/);
 /regex/.test('string');
 
-
 //Using the regex starRegex, find and extract both "Twinkle" words from the string twinkleStar.
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /twinkle/gi;  //'g' flag extracts patern more than once. 'i' flag removes case sensitivity.
 let result = twinkleStar.match(starRegex); 
+
+//Use the wildcard flag
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; //Looks for everything with 'un'
+let result = unRegex.test(exampleStr);
