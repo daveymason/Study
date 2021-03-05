8907fb3a298c615d05a92019b31ffd2cd8bb9319
Dave <daveymason@outlook.com>
2021-03-05 20:55:54 +0800

    Update Basics.php
    Added constants

--- PhpBasics/Basics.php
+++ PhpBasics/Basics.php
@@ -1,4 +1,5 @@
-<?php
+
+Variables 
 /*
 Rules for PHP variables:
 - A variable name must start with a letter or an underscore
@@ -7,8 +8,28 @@ Rules for PHP variables:
 - Variable names are case-sensitive ($name and $NAME would be two different variables)
 */
 
-
+<?php
     $name = 'John';
     $age = 25;
     echo $name;
+?>
+
+Constants are similar to variables except that they cannot be changed or undefined after they've been defined.
+Begin the name of your constant with a letter or an underscore.
+To create a constant, use the define() function:
+
+define(name, value, case-insensitive)
+
+<?php
+define("MSG", "Hello There!");
+echo MSG;
+
+//Outputs: Hello There!
+?>
+
+<?php
+define("AGE", 28);
+echo AGE;
+
+//Outputs: 28
 ?>
