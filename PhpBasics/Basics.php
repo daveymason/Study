e8c1f755da8f860ba5808f5cdb808d9d02a14cf5
Dave <daveymason@outlook.com>
2021-03-05 20:52:15 +0800

    Create Basics.php
    Added variables

---
+++ PhpBasics/Basics.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Rules for PHP variables:
+- A variable name must start with a letter or an underscore
+- A variable name cannot start with a number
+- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variable names are case-sensitive ($name and $NAME would be two different variables)
+*/
+
+
+    $name = 'John';
+    $age = 25;
+    echo $name;
+?>
