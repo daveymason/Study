4dd54191dcfe413e944642e161940a288b203085
Dave <daveymason@outlook.com>
2021-03-20 22:33:07 +0800

    Loops
    Do while.

--- PhpBasics/ControlStructs.php
+++ PhpBasics/ControlStructs.php
@@ -25,3 +25,22 @@ if (age == 18){
 } else {
     echo "Hope you enjoyed your 18th birthday";
 }
+
+//The while loop
+//The while loop executes a block of code as long as the specified condition is true.
+$i =1;
+while ($i <= 15) {
+    echo $i;
+    $i++;
+ }
+
+ //The do...while Loop
+//The do...while loop will always execute the block of code once, 
+//check the condition, and repeat the loop as long as the specified condition is true.
+
+$i = 1;
+do {  echo "Hello"; }
+while($i < 0);
+//Will still run once
+
+//The for Loop
