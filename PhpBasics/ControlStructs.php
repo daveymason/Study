d43ee2de8b9cf0d866d42aac1934607bab7765e7
Dave <daveymason@outlook.com>
2021-03-20 22:29:08 +0800

    Update ControlStructs.php
    elseif

--- PhpBasics/ControlStructs.php
+++ PhpBasics/ControlStructs.php
@@ -17,3 +17,11 @@ if ($x >= $y) {
     echo $y;
 }
 //Outputs "20"
+
+if (age == 18){
+    echo "Happy 18th Birthday";
+} elseif (age <18){
+    echo "You are not 18 yet";
+} else {
+    echo "Hope you enjoyed your 18th birthday";
+}
