4f90c24bf71e978d14fc3d20f91ee3c76324f7c2
Dave <daveymason@outlook.com>
2021-03-30 22:29:42 +0800

    Update ControlStructs.php
    Switch

--- PhpBasics/ControlStructs.php
+++ PhpBasics/ControlStructs.php
@@ -55,3 +55,34 @@ $items = array("one", "two", "three");
 foreach ($items as $item){
    echo $item ."<br />";
 }
+
+//Switch
+$today = 'Tue';
+
+switch ($today) {
+    case "Mon":
+        echo "Today is Monday.";
+        break;
+    case "Tue":
+        echo "Today is Tuesday.";
+        break;
+    case "Wed":
+        echo "Today is Wednesday.";
+        break;
+    case "Thu":
+        echo "Today is Thursday.";
+        break;
+    case "Fri":
+         echo "Today is Friday.";
+         break;
+    case "Sat":
+         echo "Today is Saturday.";
+         break;
+    case "Sun":
+         echo "Today is Sunday.";
+         break;
+    default:
+         echo "Invalid day.";
+}
+
+//outputs Today is Tuesday
