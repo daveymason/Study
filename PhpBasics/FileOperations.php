7943060692c4105a1be4e7a9d25efb946ce0668f
Dave <daveymason@outlook.com>
2021-04-05 00:12:15 +0800

    Update FileOperations.php
    Writing to a file

--- PhpBasics/FileOperations.php
+++ PhpBasics/FileOperations.php
@@ -1,4 +1,21 @@
 <?php
 //fopen will create and open a file
 //the letter gives it specific instructions
-fopen('test.txt', w);
+fopen('test.txt', 'w');
+
+//Writing to a file
+$myfile = fopen("names.txt", "w");
+
+$txt = "John\n";
+fwrite($myfile, $txt);
+$txt = "David\n";
+fwrite($myfile, $txt);
+
+fclose($myfile);
+
+/* File contains:
+John
+David
+*/
+
+//Above code creates and new file called names.txt
