9423d5bfe2abf23caecf7d72df1826d88667d48e
Dave <daveymason@outlook.com>
2021-04-10 19:37:37 +0800

    Update Inheritance.php

--- OOP/Php/Inheritance.php
+++ OOP/Php/Inheritance.php
@@ -10,3 +10,38 @@ class Dog extends Animal {
 
 $d = new Dog();
 $d->hi();
+
+//Interface
+interface AnimalInterface {
+    public function makeSound();
+}
+
+class Dog implements AnimalInterface {
+    public function makeSound() {
+        echo "Woof! <br />";
+    }
+}
+class Cat implements AnimalInterface {
+    public function makeSound() {
+        echo "Meow! <br />";
+    }
+}
+
+$myObj1 = new Dog();
+$myObj1->makeSound();
+
+$myObj2 = new Cat();
+$myObj2->makeSound();
+
+//Practice
+class Musician {
+    public $name;
+    public function toPlay() {
+
+    echo "Playing on piano";
+        }
+    }
+
+class Singer extends Musician {
+    //expand on this
+}
