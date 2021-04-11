b418e2900635696c7befb8ba3cdef6afb940dcfc
Dave <daveymason@outlook.com>
2021-04-11 14:30:47 +0800

    Update Inheritance.php

--- OOP/Php/Inheritance.php
+++ OOP/Php/Inheritance.php
@@ -78,3 +78,35 @@ abstract class Calc {
   $obj = new FixedCalc();
   echo $obj->calculate(38);
   //Outputs 42
+
+
+//Practice
+class User{
+    protected $name = 'Dave';
+    protected $age;
+
+    public function __construct($name, $age){
+        $this->name=$name;
+        $this->age=$age;
+    }
+}
+
+class Customer extends User{
+    private $balance;
+
+    public function __construct($name, $age, $balance){
+        parent::__construct($name, $age);
+        $this->balance=$balance;
+
+    }
+    public function pay($amount){
+        return $this->name . ' paid $' . $amount;
+    }
+
+    public function getBalance(){
+        return $this->balance;
+    }
+}
+
+$customer1 == new Customer('Jun', 33, 500);
+echo $customer1->getBalance(); //500
