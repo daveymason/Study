8fa3bcd97686b5fb41058dafc39cc6124106408a
Dave <daveymason@outlook.com>
2021-04-12 21:57:09 +0800

    Updated file structure

---
+++ JavaScript/basics.js
@@ -0,0 +1,21 @@
+//Variables
+var studentName = 'Davey';
+
+console.log('My name is' + studentName);
+console.log(`Hi ${studentName}!` );
+
+// The assignment operator
+=
+
+// Comparison operators
+=== "equal", !== "different", > "greater than", < "less than"
+
+// Math operators
++ "plus", - "minus", * "multiply", / "divide"
+
+//Let
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+//Let can be changed by Var can't
