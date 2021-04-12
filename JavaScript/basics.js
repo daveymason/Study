38c086b1f72f2d68b7f531f9bad69838ce3a3320
Dave <daveymason@outlook.com>
2021-04-12 22:04:11 +0800

    Update basics.js
    operators

--- JavaScript/basics.js
+++ JavaScript/basics.js
@@ -4,18 +4,42 @@ var studentName = 'Davey';
 console.log('My name is' + studentName);
 console.log(`Hi ${studentName}!` );
 
-// The assignment operator
-=
-
-// Comparison operators
-=== "equal", !== "different", > "greater than", < "less than"
-
-// Math operators
-+ "plus", - "minus", * "multiply", / "divide"
-
 //Let
 let changeMe = true;
 changeMe = false;
 console.log(changeMe);
 
-//Let can be changed by Var can't
+//Let can be changed but const can't
+const entree = 'Enchiladas';
+console.log(entree);
+entree = 'Tacos'  //Will throw an error
+
+//Operators
+
+// // The assignment operator
+// =
+
+// // Comparison operators
+// === "equal", !== "different", > "greater than", < "less than"
+
+// // Math operators
+// + "plus", - "minus", * "multiply", / "divide"
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
