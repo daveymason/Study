614e2c569c69ae9453d5ff992b438eb3f8f53fc3
Dave <daveymason@outlook.com>
2021-04-13 18:56:24 +0800

    Create tripPlanner.js

---
+++ JavaScript/Projects/tripPlanner.js
@@ -0,0 +1,7 @@
+function main() {
+    var distance = parseInt(readLine(), 10);
+    //your code goes here
+    var hours = distance / 40;
+    var minutes = hours * 60;
+    console.log(minutes);
+}   
