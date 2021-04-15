9977c917eba5177ff31d62c6c03a825f7cd3cc4b
Dave <daveymason@outlook.com>
2021-04-15 20:49:18 +0800

    Update functions.js

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -18,3 +18,20 @@ function getReminder(){
 function greetInSpanish(){
   console.log('Buenas Tardes');
 }
+
+//Function Parameters
+function sayThanks(name) {
+    console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
+  }
+  
+  sayThanks('Cole');
+
+  //Default Parameters
+  function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 ='eggs'){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+  }
+  
+  makeShoppingList();
+  
