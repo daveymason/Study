e0459c2202be3476847a00462ea511a402925297
Dave <daveymason@outlook.com>
2021-04-15 20:20:18 +0800

    Update ControlStructs.js
    Ternary operator

--- JavaScript/ControlStructs.js
+++ JavaScript/ControlStructs.js
@@ -88,6 +88,21 @@ let writingUtensil = tool || 'pen'; //sets default value of pen if tool is false
 
 console.log(`The ${writingUtensil} is mightier than the sword.`);
 
+//Ternary operator
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log ('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase ==='Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+
+
 //Switch
 var x = 3;
 switch (x) {
