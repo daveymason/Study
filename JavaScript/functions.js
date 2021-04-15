915f6be20b3b81759f33c4de6f1cbeb709aa0191
Dave <daveymason@outlook.com>
2021-04-15 20:35:19 +0800

    Create functions.js

---
+++ JavaScript/functions.js
@@ -0,0 +1,20 @@
+// Function definition
+function sayHello (name) {
+    return `Hello ${name}`;
+  }
+  
+  // Function invocation
+  sayHello('Mike');  // -> Hello Mike
+  sayHello('Jane');  // -> Hello Jane
+
+//More Examples
+getReminder();
+greetInSpanish();
+
+function getReminder(){
+  console.log('Water the plants');
+}
+
+function greetInSpanish(){
+  console.log('Buenas Tardes');
+}
