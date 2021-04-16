98070a2e8bc2866f307fea32aaa77a103e949afe
Dave <daveymason@outlook.com>
2021-04-16 21:59:58 +0800

    Update scope.js

--- JavaScript/scope.js
+++ JavaScript/scope.js
@@ -1,6 +1,5 @@
 //Global Scope
 //vars are decalred outside blocks
-
 const color = 'blue';
  
 const returnSkyColor = () => {
@@ -9,4 +8,13 @@ const returnSkyColor = () => {
  
 console.log(returnSkyColor()); // blue
 
+//Example #2
+var satellite = 'The Moon';
+var galaxy = 'The Milky Way';
+var stars = 'North Star';
 
+const callMyNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
