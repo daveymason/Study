eb953c76182f13093a9b94e92fc3c86afc0caa79
Dave <daveymason@outlook.com>
2021-04-16 21:58:15 +0800

    Create scope.js

---
+++ JavaScript/scope.js
@@ -0,0 +1,12 @@
+//Global Scope
+//vars are decalred outside blocks
+
+const color = 'blue';
+ 
+const returnSkyColor = () => {
+  return color; // blue 
+};
+ 
+console.log(returnSkyColor()); // blue
+
+
