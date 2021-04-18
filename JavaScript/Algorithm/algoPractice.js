9f7fe4aa271774174683b32d3fc7e76563b39459
Dave <daveymason@outlook.com>
2021-04-18 20:41:27 +0800

    Update algoPractice.js

--- JavaScript/Algorithm/algoPractice.js
+++ JavaScript/Algorithm/algoPractice.js
@@ -7,3 +7,11 @@ function centuryFromYear(year) {
     }
     console.log(result);
 }
+
+//Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+   let fahrenheit = celsius * (9/5) + 32;
+   return fahrenheit;
+ }
+ 
+ convertToF(30);
