1ece033bcd57d8bf74de2a5784fca3e1966d0028
Davey Mason <daveymason@outlook.com>
2021-05-27 21:31:53 +0800

    Update longestWordInString.js

--- JavaScript/Algorithm/longestWordInString.js
+++ JavaScript/Algorithm/longestWordInString.js
@@ -1,11 +1,5 @@
-function findLongestWordLength(str) {
-    return Math.max(...str.split(" ").map(word => word.length));
+function findLongestWord(str) {
+    return Math.max(...str.split(" ").map(word =>word.length));
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
-
-function findLongestWordLength(str) {
-    return Math.max(...str.split(" ").map(word => word.length));
-}
-
-findLongestWordLength("Ad a few words here for the fuckint hings to work");
+findLongestWord("There is some stuff inside this function");
