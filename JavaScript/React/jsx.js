38f1cf0102a2d262fb2dc0a094efc2f3f8b6cc35
Davey Mason <daveymason@outlook.com>
2021-06-13 15:51:54 +0800

    Create jsx.js

---
+++ JavaScript/React/jsx.js
@@ -0,0 +1,15 @@
+// Basic JSX
+const JSX = <h1>Hello JSX!</h1>;
+
+//More complex JSX
+const JSX =
+<div>
+  <h1>Header</h1>
+  <p>Paragrapgh</p>
+  <ul>
+    <li>list item</li>
+    <li>list item</li>
+    <li>list item</li>
+  </ul>
+</div>
+;
