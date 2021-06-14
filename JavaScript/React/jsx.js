d846c4abbd7c5362fbfedec2e0d85a0784687fb9
Davey Mason <daveymason@outlook.com>
2021-06-14 17:03:40 +0800

    Update jsx.js

--- JavaScript/React/jsx.js
+++ JavaScript/React/jsx.js
@@ -47,3 +47,67 @@ const JSX = (
         </div>
     )
   }
+
+//Another example
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+    <div>
+      <h1>Hello React!</h1>
+    </div>
+    );
+  }
+};
+
+//Parent & Child basic components
+return (
+  <App>
+   <Navbar />
+   <Dashboard />
+   <Footer />
+  </App>
+ )
+
+//Parent & Child Components
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+          <ChildComponent />
+      </div>
+    );
+  }
+};
+
+//Render example
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+          <Fruits />
+          <Vegetables />
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
