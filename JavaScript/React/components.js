dd3014ab6a6c9c496f68e6032a0a472ee9017a6b
Davey Mason <daveymason@outlook.com>
2021-06-14 18:34:49 +0800

    Update components.js

--- JavaScript/React/components.js
+++ JavaScript/React/components.js
@@ -70,3 +70,44 @@ class TypesOfFood extends React.Component {
 };
 
 ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
+
+//Create own component
+class MyComponent extends React.Component{
+    constructor(props){
+      super(props);
+    }
+  
+    render () {
+      return (
+        <div id='challenge-node'>
+          <h1>My First React Component!</h1>
+        </div>
+      )
+    };
+  
+  }
+  
+  ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'))
+
+  //Pass Props to a Stateless Functional Component
+  const CurrentDate = (props) => {
+    return (
+      <div>
+        <p>The current date is: {props.date}</p>
+      </div>
+    );
+  };
+  
+  class Calendar extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h3>What date is it?</h3>
+          <CurrentDate date={Date()}/>
+        </div>
+      );
+    }
+  };
