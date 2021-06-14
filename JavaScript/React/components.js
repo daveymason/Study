b383f594e880080a8ba52711880b15b142686d9f
Davey Mason <daveymason@outlook.com>
2021-06-14 17:04:25 +0800

    Updated file structure for components

---
+++ JavaScript/React/components.js
@@ -0,0 +1,72 @@
+  //Stateless functional component
+  const MyComponent = function() {
+    return (
+        <div className='someDiv'>
+        <p>'Hello there'</p>
+        </div>
+    )
+  }
+
+//Another example
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+    <div>
+      <h1>Hello React!</h1>
+    </div>
+    );
+  }
+};
+
+//Parent & Child basic components
+return (
+  <App>
+   <Navbar />
+   <Dashboard />
+   <Footer />
+  </App>
+ )
+
+//Parent & Child Components
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+          <ChildComponent />
+      </div>
+    );
+  }
+};
+
+//Render example
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+          <Fruits />
+          <Vegetables />
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
