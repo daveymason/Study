e9a2baddca892e7f463fbc315783534e9d36aced
Davey Mason <daveymason@outlook.com>
2021-06-16 20:52:20 +0800

    Update components.js

--- JavaScript/React/components.js
+++ JavaScript/React/components.js
@@ -132,3 +132,22 @@ class MyComponent extends React.Component{
       );
     }
   };
+
+
+  //Override default props
+  const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  }
+  
+  Items.defaultProps = {
+    quantity: 0
+  }
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return <Items quantity={10}/>
+    }
+  };
