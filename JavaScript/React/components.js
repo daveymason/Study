c41fd91aeb80ace026669455b4501178071a2b8b
Davey Mason <daveymason@outlook.com>
2021-06-16 20:45:02 +0800

    Update components.js

--- JavaScript/React/components.js
+++ JavaScript/React/components.js
@@ -111,3 +111,24 @@ class MyComponent extends React.Component{
       );
     }
   };
+
+  const List = (props) => {
+    return <p>{props.tasks.join(', ')}</p>
+  };
+  
+  class ToDo extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>To Do Lists</h1>
+          <h2>Today</h2>
+          <List tasks={["walk dog", "workout"]}/>
+          <h2>Tomorrow</h2>
+          <List tasks={["walk cat", "workin", "eat"]}/>
+        </div>
+      );
+    }
+  };
