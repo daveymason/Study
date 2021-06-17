f34adab49df368aa73f2aa5bffc9b343f008bb40
Davey Mason <daveymason@outlook.com>
2021-06-17 17:03:23 +0800

    Update state.js

--- JavaScript/React/state.js
+++ JavaScript/React/state.js
@@ -12,7 +12,7 @@ class StatefulComponent extends React.Component {
       );
     }
   };
-  
+
 //Render Props to UI
 const CurrentDate = (props) => {
     return (
@@ -55,3 +55,28 @@ const CurrentDate = (props) => {
     }
   };
 
+//setState
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'Initial State'
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      // Change code below this line
+        this.setState({
+          name: 'React Rocks!'
+        })
+      // Change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.handleClick}>Click Me</button>
+          <h1>{this.state.name}</h1>
+        </div>
+      );
+    }
+  };
