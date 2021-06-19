c502fa4e2331be4aa8cd473c78d5bf6020616d88
Davey Mason <daveymason@outlook.com>
2021-06-19 14:08:34 +0800

    Update state.js

--- JavaScript/React/state.js
+++ JavaScript/React/state.js
@@ -80,3 +80,41 @@ class MyComponent extends React.Component {
       );
     }
   };
+
+  //Toggle element with state
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        visibility: false
+      };
+      this.toggleVisibility = this.toggleVisibility.bind(this);
+    }
+    // 
+    toggleVisibility() {
+      this.setState(state => {
+        if (state.visibility === true) {
+           return { visibility: false };
+         } else {
+           return { visibility: true };
+        }
+      });
+    }
+    // 
+    render() {
+      if (this.state.visibility) {
+        return (
+          <div>
+            <button onClick={this.toggleVisibility}>Click Me</button>
+            <h1>Now you see me!</h1>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <button onClick={this.toggleVisibility}>Click Me</button>
+          </div>
+        );
+      }
+    }
+  }
