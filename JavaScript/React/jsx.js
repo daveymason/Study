542057faeb54b54f1f6151696db4a847346f7daf
Davey Mason <daveymason@outlook.com>
2021-06-19 14:58:28 +0800

    Update jsx.js

--- JavaScript/React/jsx.js
+++ JavaScript/React/jsx.js
@@ -91,3 +91,27 @@ const headline = (
     { age >= drinkingAge ? 'Buy Drink' : 'Do Teen Stuff' }
   </h1>
 );
+
+//&&
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      { !judgmental && <li> Nacho Cheez Straight Out The Jar</li>} {/* Will only render if is not judgmental */}
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+	favoriteFoods, 
+	document.getElementById('app')
+);
