8cafe10ebf5ac05314a4ec4223155d821b653bbf
Davey Mason <daveymason@outlook.com>
2021-06-21 18:45:23 +0800

    Update destructuring.js

--- JavaScript/ES6/destructuring.js
+++ JavaScript/ES6/destructuring.js
@@ -14,3 +14,16 @@ console.log(age)//undefined
 const numbers = [1,2,3];
 [num1 , , num3] = numbers;
 console.log(num1, num3);
+
+
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
+  
+  //Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+  const half = ({max, min}) => (max + min) / 2.0;
