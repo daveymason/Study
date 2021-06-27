9bba7ef0ffbe347102dd85f64b6bfd636dfa8fd0
Davey Mason <daveymason@outlook.com>
2021-06-27 18:15:46 +0800

    Conditional Rendering

--- JavaScript/React/lifeCycleMethods.js
+++ JavaScript/React/lifeCycleMethods.js
@@ -1,3 +1,4 @@
+//componentWillMount
 class MyComponent extends React.Component {
     constructor(props) {
       super(props);
@@ -11,3 +12,109 @@ class MyComponent extends React.Component {
       return <div />
     }
   };
+
+//componentDidMount
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeUsers: null
+      };
+    }
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          activeUsers: 1273
+        });
+      }, 2500);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Active Users: {this.state.activeUsers}</h1>
+        </div>
+      );
+    }
+  }
+
+  //Event Listeners
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: ''
+      };
+      this.handleEnter = this.handleEnter.bind(this);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    componentDidMount() {
+      document.addEventListener(
+        "keydown", this.handleKeyPress )
+    }
+    componentWillUnmount() {
+      document.removeEventListener(
+        "keydown", this.handleKeyPress )
+    }
+
+    handleEnter() {
+      this.setState((state) => ({
+        message: state.message + 'You pressed the enter key! '
+      }));
+    }
+    handleKeyPress(event) {
+      if (event.keyCode === 13) {
+        this.handleEnter();
+      }
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.message}</h1>
+        </div>
+      );
+    }
+  };
+
+  //shouldComponentUpdate
+  class OnlyEvens extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+      console.log('Should I update?');
+      if (nextProps.value % 2 == 0) {
+          return true;
+        }
+        return false;
+    }
+    componentDidUpdate() {
+      console.log('Component re-rendered.');
+    }
+    render() {
+      return <h1>{this.props.value}</h1>;
+    }
+  }
+  
+  class Controller extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 0
+      };
+      this.addValue = this.addValue.bind(this);
+    }
+    addValue() {
+      this.setState(state => ({
+        value: state.value + 1
+      }));
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.addValue}>Add</button>
+          <OnlyEvens value={this.state.value} />
+        </div>
+      );
+    }
+  }
