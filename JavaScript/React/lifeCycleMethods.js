4b93e26924ad508239bccf057d570eee2ee37659
Davey Mason <daveymason@outlook.com>
2021-06-27 16:01:05 +0800

    Create lifeCycleMethods.js

---
+++ JavaScript/React/lifeCycleMethods.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+        <div>
+        console.log('Mounted')
+        </div>
+    }
+    render() {
+      return <div />
+    }
+  };
