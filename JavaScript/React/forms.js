96e8540a5e355d39bd06634a9caa05d0adbc7c07
Davey Mason <daveymason@outlook.com>
2021-07-02 21:24:37 +0800

    Update forms.js

--- JavaScript/React/forms.js
+++ JavaScript/React/forms.js
@@ -89,3 +89,33 @@ class App extends Component {
         )
     }
 }
+
+//Form practice
+class ControlledInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: ''
+      };
+    this.handleChange = this.handleChange.bind(this)
+    }
+
+  handleChange (e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+  
+    render() {
+      return (
+        <div>
+          <input 
+          value={this.state.input}
+          onChange={this.handleChange}
+          />
+          <h4>Controlled Input:</h4>
+          <p>{this.state.input}</p>
+        </div>
+      );
+    }
+  };
