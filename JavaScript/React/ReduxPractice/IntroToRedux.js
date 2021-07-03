8ab3432723ef332ee7ad4ad7b5819b5709a6fc44
Davey Mason <daveymason@outlook.com>
2021-07-03 10:24:04 +0800

    Update IntroToRedux.js

--- JavaScript/React/ReduxPractice/IntroToRedux.js
+++ JavaScript/React/ReduxPractice/IntroToRedux.js
@@ -4,3 +4,10 @@ const reducer = (state = 5) => {
 }
 
 const store = Redux.createStore(reducer)
+
+//Get State from Redux store
+const store = Redux.createStore(
+    (state = 5) => state
+  );
+  
+  const currentState = store.getState()
