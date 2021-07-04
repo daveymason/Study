41d31afa6ab4fc1bd89249da0442fc7df546aeda
Davey Mason <daveymason@outlook.com>
2021-07-04 11:02:32 +0800

    Update arrays.js

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -338,3 +338,49 @@ const numbers = [1,2,3];
 const doubleNum = numbers.map(n => n *2);
 console.log(numbers); //1,2,3
 console.log(doubleNum);//2,4,6
+
+//Map and filter 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    };
+  }
+  render() {
+    const usersOnline = this.state.users.filter(user => user.online === true); // Change this line
+    const renderOnline = usersOnline.map(user => <li key={user.username}>{user.username}</li>)
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
+} 
+  
