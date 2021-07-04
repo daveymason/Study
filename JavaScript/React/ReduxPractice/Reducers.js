887f6c86235f1506910a2227eed351069ef12d2a
Davey Mason <daveymason@outlook.com>
2021-07-04 12:25:46 +0800

    Update Reducers.js

--- JavaScript/React/ReduxPractice/Reducers.js
+++ JavaScript/React/ReduxPractice/Reducers.js
@@ -20,3 +20,42 @@ const defaultState = {
       type: 'LOGIN'
     }
   };
+
+  //Using Switch 
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+ 
+switch (action.type) {
+    case "LOGIN":
+      return {
+        authenticated: true
+      };
+
+    case "LOGOUT":
+      return {
+        authenticated: false
+      };
+
+    default:
+      return defaultState;
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
