1c0272efa170412cd07e4284dc81490a172c5223
Davey Mason <daveymason@outlook.com>
2022-07-23 17:08:26 +0200

    Create data_types.py

---
+++ Python/data_types.py
@@ -0,0 +1,73 @@
+#Declare Variables
+x = 15
+y = 10
+z = x + y
+print(z)
+
+"""
+Python has a number of data types, including strings, numbers, and Booleans.
+"""
+#Strings
+x = "Hello World"
+print(x)
+
+"""
+Strings can be added together with the + operator:
+"""
+x = "Hello"
+y = "World"
+z = x + y
+print(z)
+
+"""
+Strings can also be multiplied by integers:
+"""
+x = "Hello"
+y = "World"
+z = x * 3
+print(z)
+
+"""
+Primative data types
+"""
+#Integers
+x = 10
+print(type(x))
+
+#Boolean
+x = True
+print(type(x))
+
+#Strings
+x = "Hello"
+print(type(x))
+
+#float
+x = 1.0
+print(type(x))
+
+
+"""
+Composite data types
+"""
+#List
+x = ["Seattle", "Tacoma", "Bellevue"]
+print(type(x))
+
+#Tuple
+x = ("Seattle", "Tacoma", "Bellevue")   #Note the comma
+print(type(x))
+
+#Set
+x = {"Seattle", "Tacoma", "Bellevue"}
+print(type(x))
+
+#Dictionary
+x = {"name": "Seattle", "state": "WA"}
+print(type(x))
+
+
+
+
+
+
