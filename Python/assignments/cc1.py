7de02ccfdf34b706c668f61274eb06b2afe03c13
Davey Mason <daveymason@outlook.com>
2022-07-29 23:03:45 +0200

    Assignments

---
+++ Python/assignments/cc1.py
@@ -0,0 +1,18 @@
+#Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 4.5
+print(type(b))
+
+c = "Hello"
+print(type(c))
+
+d = a + b
+print(type(d))
+
+
+
